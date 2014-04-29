4b7427e94e4aaa5221bf528533a88d44408916af
Antonio Jesús Sánchez Padial <antonio.sanchez@inia.es>
2014-04-29 08:52:51 +0200

    Initial commit.

---
+++ JournalTemplatePlugin.inc.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file GoogleAnalyticsPlugin.inc.php
+ *
+ * Copyright (c) 2003-2011 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class GoogleAnalyticsPlugin
+ * @ingroup plugins_generic_googleAnalytics
+ *
+ * @brief Google Analytics plugin class
+ */
+
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+class JournalTemplatePlugin extends GenericPlugin {
+	/**
+	 * Called as a plugin is registered to the registry
+	 * @param $category String Name of category plugin was registered to
+	 * @return boolean True iff plugin initialized successfully; if false,
+	 * 	the plugin will not be registered.
+	 */
+	function register($category, $path) {
+		$success = parent::register($category, $path);
+//		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
+		if ($success && $this->getEnabled()) {
+				HookRegistry::register('Templates::Index::journal', array($this, 'callback'));
+	      
+	      return true;
+	 
+	  }
+	  return false;
+	}	
+	function getName(){
+		return 'JournalTemplatePlugin';
+	}	
+	function getDisplayName() {
+		return Locale::translate('plugins.generic.journalTemplate.displayName');
+	}
+
+	function getDescription() {
+		return Locale::translate('plugins.generic.journalTemplate.description');
+	}
+
+	
+	
+
+	/**
+	 * Insert a template in journal.tpl
+	 */
+	function callback($hookName, $args) {
+		$params =& $args[0];
+		$smarty =& $args[1];
+		$output =& $args[2];
+//print ($this->getTemplatePath());
+		$output = $smarty->fetch($this->getTemplatePath() . 'journalTemplate.tpl');
+		return false;
+	}
+
+	
+}
+?>
---
+++ journalTemplate.tpl
@@ -0,0 +1,6 @@
+ <div style="text-align: center;"><a href="{url page="issue" op="current"}">{translate key="navigation.current"}</a></div>
+ <br />
+ <br />
+  <div style="text-align: center;"><a href="{url page="issue" op="futureIssues"}">{translate key="navigation.futureIssues"}</a></div>
+</div>
+<div style="overflow:hidden;width:100%"> </div>
